abc61cd29939306200663da4172e8a780064f40b
Juan Antona <juan.antonadelval@gmail.com>
2023-10-12 09:08:00 +0000

    test(Stats): Creates stats.ts and stats.test.ts

---
+++ src/stats.test.ts
@@ -0,0 +1,18 @@
+import { average, sum } from "./stats";
+
+let result, expected;
+result = sum([1, 2, 3]);
+expected = 6;
+if (result === expected) {
+  console.log("✅");
+} else {
+  throw new Error(`❌ ${result} is not equal to ${expected}`);
+}
+
+result = average([1, 2, 3]);
+expected = 2;
+if (result === expected) {
+  console.log("✅");
+} else {
+  throw new Error(`❌ ${result} is not equal to ${expected}`);
+}
