48b35ca4296ed80023341d449f7dc081acd1cc6e
Juan Antona <juan.antonadelval@gmail.com>
2023-10-18 19:54:58 +0000

    test(Stats): Encapsulates the tests

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,11 +1,14 @@
 import { average, sum } from "./stats";
-import { expect } from "./testLib";
+import { expect, test } from "./testLib";
 
-let result, expected;
-result = sum([1, 2, 3]);
-expected = 6;
-expect(result).toBe(expected);
+test("The sum of the numbers of the array", function () {
+  const result = sum([1, 2, 3]);
+  const expected = 6;
+  expect(result).toBe(expected);
+});
 
-result = average([1, 2, 3]);
-expected = 2;
-expect(result).toBe(expected);
+test("The average of the numbers of the array", function () {
+  const result = average([1, 2, 3]);
+  const expected = 2;
+  expect(result).toBe(expected);
+});
--- src/testLib.ts
+++ src/testLib.ts
@@ -1,11 +1,19 @@
 export function expect<T>(result: T) {
   return {
     toBe: function (expected: T) {
-      if (result === expected) {
-        console.log("✅");
-      } else {
-        throw new Error(`❌ ${result} is not equal to ${expected}`);
+      if (result !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`);
       }
     },
   };
+}
+
+export function test(description: string, callback: () => void) {
+  try {
+    callback();
+    console.log(`✅  ${description}`);
+  } catch (error) {
+    console.log(`❌  ${description}`);
+    console.log(error.message);
+  }
 }
